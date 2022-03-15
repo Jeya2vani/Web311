11094752c2c48d227eca21dd3ee3550e17c33623
Jeya2vani <99476100+Jeya2vani@users.noreply.github.com>
2022-03-15 16:32:15 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -5,16 +5,33 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 @Getter
 @Setter
 @ToString
-
 @Document(collection = "Employee")
 public class Employee {
     @Id
     private int id;
     private String name;
     private String department;
-    private int salary;
+    private float salary;
+    private float percentage;
+
+   public float getPercentage() {
+        percentage = (salary / mini()) * 100;
+        return percentage;
+    }
+    private float mini() {
+        float[] sal={salary};
+        float temp=sal[0];
+        for (int i = 0; i < sal.length; i++) {
+            if (temp > sal[i]) {
+                temp = sal[i];
+            }
+        }
+        return temp;
+    }
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
 }
